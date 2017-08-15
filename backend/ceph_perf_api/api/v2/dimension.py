b1411ecdf5787dd39975b05ed6f2f76cc7d90b4a
zhouning2 <zhouning2@lenovo.com>
2017-08-15 16:38:18 +0800

    add 2 api

--- backend/ceph_perf_api/api/v2/dimension.py
+++ backend/ceph_perf_api/api/v2/dimension.py
@@ -3,22 +3,63 @@ import urls
 from django.views import generic
 from ceph_perf_api import utils
 
+
 DATA = {
     "iostat": {
-    'wMBs': 'mx-',
-    'tps': 'y*-',
-    'avgrqsz': 'co-',
+        'wrqms': 'r*-',
+        'avgrqsz': 'g*-',
+        'r_await': 'b*-',
+        'await': 'c*-',
+        'ws': 'm*-',
+        'avgqusz': 'y*-',
+        'svctm': 'k*-',
+        'rMBs': 'ro--',
+        'wMBs': 'go--',
+        'rrqms': 'bo--',
+        'rs': 'co--',
+        'tps': 'mo--',
+        'util': 'yo--',
+        'w_await': 'ko--',
     },
     "sarcpu": {
-    'iowait': 'mx-',
-    'irq': 'y*-',
-    'soft': 'co-',
+        'usr': 'r*-',
+        'nice': 'g*-',
+        'sys': 'b*-',
+        'iowait': 'c*-',
+        'steal': 'm*-',
+        'irq': 'y*-',
+        'soft': 'k*-',
+        'guest': 'ro--',
+        'gnice': 'go--',
+        'idle': 'bo--',
+    },
+    "sarmem": {
+        'kbmemfree': 'r*-',
+        'kbmemused': 'g*-',
+        'memused': 'b*-',
+        'kbbuffers': 'c*-',
+        'kbcached': 'm*-',
+        'kbcommit': 'y*-',
+        'commit': 'k*-',
+        'kbactive': 'ro--',
+        'kbinact': 'go--',
+        'kbdirty': 'bo--',
+    },
+    "sarnic": {
+        'rxpcks': 'r*-',
+        'txpcks': 'g*-',
+        'rxkBs': 'b*-',
+        'txkBs': 'c*-',
+        'rxcmps': 'm*-',
+        'txcmps': 'y*-',
+        'rxmcsts': 'k*-',
     },
 }
 
+
 @urls.register
 class Dimension(generic.View):
-    url_regex = r'^dimension$'
+    url_regex = r'^metric$'
 
     @utils.json_response
     def get(self, request):
--- backend/ceph_perf_api/api/v2/result.py
+++ backend/ceph_perf_api/api/v2/result.py
@@ -4,7 +4,11 @@ from django.views import generic
 from ceph_perf_api import utils
 from fiotest import models
 
-    
+
+def foreign_convert(model):
+    return "%s %s" % (model.jobid.name, model.jobid.time)
+
+
 @urls.register
 class FIOTEST(generic.View):
     url_regex = r'^fiotest$'
@@ -17,13 +21,126 @@ class FIOTEST(generic.View):
             f[key] = value[0]
         print f
         result = models.Result.objects.filter(**f).all()
-        for item in result:
-            print item.jobid.time
-
-        def foreign_convert(model):
-            return "%s %s" % (model.jobid.name, model.jobid.time)
 
         d = utils.query_to_dict(result, "jobid", foreign_convert)
         return {"total": len(d), "data": d}
 
+
+def mk_check(n, array):
+    check = []
+    for i in range(n):
+        check.append(False)
+    for i in array:
+        check[i] = True
+    return check
+
+
+def jobs(array):
+    result = models.Result.objects.all()
+    d = utils.query_to_dict(result, "jobid", foreign_convert)
+    total = len(d)
+    check = mk_check(total, array)
+
+    dimension = {
+        'iops': {},
+        'lat': {},
+        'bw': {},
+    }
+
+    x_label = {}
+    keys = ['iodepth', 'numberjob', 'imagenum', 'clientnum']
+    for item in [d[i] for i in range(total) if check[i]]:
+        name = item['blocksize']
+        for key in keys:
+            name += '_%s%s' % (key, item[key])
+        name += '_' + item['readwrite']
+        x_label[name] = True
+
+        for key, _ in dimension.items():
+            try:
+                dimension[key][item['jobid']].update({name: item[key]})
+            except KeyError:
+                dimension[key][item['jobid']] = {}
+                dimension[key][item['jobid']].update({name: item[key]})
+
+    x_label = [k for k, v in x_label.items()]
+    for key, _ in dimension.items():
+        dimension[key]['name'] = x_label
+
+    return dimension
+
+
+def jobs2(array):
+    result = models.Result.objects.all()
+    d = utils.query_to_dict(result, "jobid", foreign_convert)
+    total = len(d)
+    check = mk_check(total, array)
+
+    dimension = {
+        'iops': {},
+        'lat': {},
+        'bw': {},
+    }
+
+    keys = ['iodepth', 'numberjob', 'imagenum', 'clientnum']
+    for item in [d[i] for i in range(total) if check[i]]:
+        name = item['blocksize']
+        for key in keys:
+            name += '_%s%s' % (key, item[key])
+        name += '_' + item['readwrite']
+        for key, _ in dimension.items():
+            if not dimension[key].get(name):
+                dimension[key][name] = {}
+
+        for key, _ in dimension.items():
+            try:
+                dimension[key][name][item['jobid']] = item.get(key)
+            except KeyError:
+                dimension[key][name][item['jobid']] = []
+                dimension[key][name][item['jobid']] = item.get(key)
+
+    for key, value in dimension.items():
+        items = value.items()
+        items.sort()
+        l = []
+        for k, _ in items:
+            dimension[key][k]['name'] = k
+            l.append(dimension[key][k])
+        dimension[key] = l
+
+    return dimension
+
+
+def results(array):
+    result = models.Result.objects.all()
+    d = utils.query_to_dict(result, "jobid", foreign_convert)
+    total = len(d)
+    check = mk_check(total, array)
+
+    dimension = {
+        'iops': [],
+        'lat': [],
+        'bw': [],
+    }
+
+    for item in [d[i] for i in range(total) if check[i]]:
+        for key, _ in dimension.items():
+            dimension[key].append({'name': item.get('case_name'), key: item.get(key)})
+    return dimension
+
+
+@urls.register
+class Figure(generic.View):
+    url_regex = r'^figure$'
+
+    @utils.json_response
+    def get(self, request):
+        body = request.DATA
+        print body
+        type = body['type']
+        array = body['id']
+        if type == "job":
+            return jobs(array)
+        elif type == "result":
+            return results(array)
 
